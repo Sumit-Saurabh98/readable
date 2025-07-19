c5d195c6cd0fa08b7694bc3b77b97c50c230965e
sumit-saurabh98 <sumitsaurabh112@gamil.com>
2025-07-20 00:19:42 +0530

    added lession details

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,56 @@
+import { EmptyState } from "@/components/general/EmptyState"
+import { getAllCourses } from "../data/course/get-all-courses"
+import { getEnrolledCourses } from "../data/user/get-enrolled-courses"
+import { PublicCourseCard } from "../(public)/_components/PublicCourseCard"
+import Link from "next/link"
+
+
+export default async function DashboardPage(){
+
+const [courses, enrolledCourses] = await Promise.all([
+  getAllCourses(), getEnrolledCourses()
+])
+
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-bold">Enrolled Courses</h1>
+        <p className="text-muted-foreground">Here you can see all the courses you have access to</p>
+      </div>
+
+      {enrolledCourses.length === 0 ? (
+        <EmptyState title="No courses purchaged" description="You have not purchased any courses yet" buttonText="Browse Courses" href="/courses"/>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {
+            enrolledCourses.map((course) => (
+              <Link key={course.Course.id} href={`/dashboard/${course.Course.slug}`}>
+              {course.Course.title}
+              </Link>
+            ))
+          }
+        </div>
+      )}
+
+
+      <section className="mt-10">
+      <div className="flex flex-col gap-2 mb-5">
+        <h1 className="text-3xl font-bold">Availabe Courses</h1>
+        <p className="text-muted-foreground">Here you can see all the courses you can purchase</p>
+      </div>
+
+      {courses.filter((course) =>!enrolledCourses.some(({Course:enrolled}) =>enrolled.id === course.id)).length === 0 ? (
+        <EmptyState title="No courses available" description="There are no courses available at the moment" buttonText="Browse Courses" href="/courses"/>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {
+            courses.filter((course) =>!enrolledCourses.some(({Course:enrolled}) =>enrolled.id === course.id)).map((course) => (
+              <PublicCourseCard key={course.id} data={course}/>
+            ))
+          }
+        </div>
+      )}
+      </section>
+    </>
+  )
+}
---
+++ app/data/course/get-course-sidebar-data.ts
@@ -0,0 +1,72 @@
+import "server-only"
+import { requireUser } from "../user/require-user";
+import { prisma } from "@/lib/db";
+import { notFound } from "next/navigation";
+
+export async function getCourseSidebarData(slug: string){
+    const session = await requireUser();
+
+    const course = await prisma.course.findUnique({
+        where: {
+            slug: slug
+        },
+        select: {
+            id: true,
+            title: true,
+            smallDescription: true,
+            duration: true,
+            level: true,
+            status: true,
+            price: true,
+            fileKey: true,
+            slug: true,
+            category: true,
+            chapter: {
+                orderBy: {
+                    position: "asc"
+                },
+                select: {
+                    id: true,
+                    title: true,
+                    position: true,
+                    lessons: {
+                        orderBy: {
+                            position: "asc"
+                        },
+                        select: {
+                            id: true,
+                            title: true,
+                            description: true,
+                            thumbnailKey: true,
+                            videoKey: true,
+                            position: true
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    if(!course){
+        return notFound();
+    }
+
+    const enrollment = await prisma.enrollment.findUnique({
+        where: {
+            userId_courseId: {
+                userId: session.id,
+                courseId: course.id
+            }
+        }
+    })
+
+    if(!enrollment || enrollment.status !== "Active"){
+        return notFound();
+    }
+
+    return {
+        course
+    }
+}
+
+export type CourseSidebarDataType = Awaited<ReturnType<typeof getCourseSidebarData>>
